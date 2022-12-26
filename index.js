64c22d2178ce782571eb34de23f7fe1e9f645993
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2022-12-26 16:44:16 +0200

    added profile endpoints(my library + user info)

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const api = new Spotify({
     redirectUri: 'http://localhost:3000/callback'
 });
 
-const token = 'BQBsPkpnlrJjEJM-VQe-f-bctabRHwqgZ9Aag6nCH350qcKfyeEd6kXWKHpX_ayP7O5AbP68iXHB4t4tk4_GylUgPHh6qLdt2V4rvqakThajsTTryXftvy2smJkAtldwTKXpLM2Tko0x9ZVS02j0u25R0pOyPIv5fp_Z_k2OjEQjj0unlI3ujySsEY8gnE-xSOOlgn_FMYMJqI20pNivhYQJmtj0BfUpTKLp';
+const token = 'BQDSK1-bZZX6Ex1t2rpJiCH9Yefh706A6RwcM_fnD_DZwnxQBAwCKR-rzVmvWjSdFeX7LN6UwA72db0YBb3fLZHaeNwmb7m1rR4S_Cb9SEfvacAeM0B5UZ96d2R5UQSJ5n5xZtsjeOdm1Gm5B9Zkllv984_qM3fpNWxR2OgQNjTQ1Ja4TUXMxgZ9aePJ5wqn5y5cxLLADV2PkiiFU4H6oNPmtd50aat_rv0g';
 
 const formattrack = (res) => {
     return {
@@ -19,6 +19,15 @@ const formattrack = (res) => {
         id: res.id,
         artist: res.artists[0].name,
         image: res.album.images[0].url
+    };
+};
+
+const formatalbum = (res) => {
+    return {
+        name: res.name,
+        id: res.id,
+        artist: res.artists[0].name,
+        image: res.images[0].url
     };
 };
 
@@ -35,6 +44,42 @@ const formatsearch = (res) => {
     return songs;
 }
 
+const formatlikedpls = (res) => {
+    const pls = [];
+    res.items.forEach(el => {
+        pls.push({
+            name: el.name,
+            owner: el.owner.display_name,
+            image: el.images[0].url,
+            id: el.id
+        });
+    });
+    return {
+        pls: pls
+    }
+};
+
+const formatusertrs = (res) => {
+    const trs = [];
+    res.items.forEach(el => {
+        trs.push(formattrack(el.track));
+    });
+    return {
+        trs: trs
+    }
+};
+
+const formatuserals = (res) => {
+    const als = [];
+    res.items.forEach(el => {
+        als.push(formatalbum(el.album));
+    });
+    return {
+        als: als
+    }
+};
+
+
 app.get('/login', (req, res) => {
     res.redirect(api.createAuthorizeURL(['playlist-read-private', 'user-read-private', 'user-library-read'], 'state'));
 });
@@ -45,16 +90,16 @@ app.get('/callback', (req, res) => {
     });
 });
 
-app.get('/yt/:id', (req, res) => {
-    ytdl(`https://www.youtube.com/watch?v=${req.params.id}`, { filter: 'audioonly', quality: 'highestaudio' })
-        .pipe(createWriteStream(`${req.params.id}.mp3`))
-        .on('finish', () => res.download(`${req.params.id}.mp3`));
-});
-
 app.get('/search/:term', async (req, res) => {
     const { videos } = await search(req.params.term);
     console.log(videos);
     res.send(formatsearch(videos));
+});
+
+app.get('/yt/:id', (req, res) => {
+    ytdl(`https://www.youtube.com/watch?v=${req.params.id}`, { filter: 'audioonly', quality: 'highestaudio' })
+        .pipe(createWriteStream(`${req.params.id}.mp3`))
+        .on('finish', () => res.download(`${req.params.id}.mp3`));
 });
 
 app.get('/spot/:id', async (req, res) => {
@@ -72,10 +117,28 @@ app.get('/spot/:id', async (req, res) => {
         .on('finish', () => res.download(`${id}.mp3`));
 });
 
-app.get('/yt/:id', (req, res) => {
-    ytdl(`https://www.youtube.com/watch?v=${req.params.id}`, { filter: 'audioonly', quality: 'highestaudio' })
-        .pipe(createWriteStream(`${req.params.id}.mp3`))
-        .on('finish', () => res.download(`${req.params.id}.mp3`));
+app.get('/me', async (req, res) => {
+    api.setAccessToken(token);
+    const data = await api.getMe();
+    res.json({ name: data.body.display_name, image: data.body.images[0].url });
+});
+
+app.get('/usertrs', async (req, res) => {//liked tracks
+    api.setAccessToken(token);
+    const data = await api.getMySavedTracks({ limit: 50 });
+    res.json(formatusertrs(data.body));
+});
+
+app.get('/userpls', async (req, res) => {//this has all the playlists(made + following)
+    api.setAccessToken(token);
+    const data = await api.getUserPlaylists({ limit: 50 });
+    res.json(formatlikedpls(data.body));
+});
+
+app.get('/userals', async (req, res) => {
+    api.setAccessToken(token);
+    const data = await api.getMySavedAlbums({ limit: 50 });
+    res.json(formatuserals(data.body));
 });
 
 app.listen(3000, () => console.log('app listening'));
